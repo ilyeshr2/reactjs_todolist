211b9d69a868547adaefde3c3b717207308a3c45
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-07-25 13:14:53 -0400

    Update TodoCard.jsx

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default function TodoCard(props) {
 
-  const {item} = props//getting the data from the parent component
+  const {item} = props
   const {handleDelTodos} = props
     
   return (
       <li className='todoItem'>
         {item}
-      <div className='actionsContainer'>
-        <button onClick={() => {
-          console.log(item)
-          handleDelTodos(item)}}>
+        <div className='actionsContainer'>
           <i className="fa-solid fa-pen-to-square"></i>
-        </button>
-        <i className="fa-solid fa-trash"></i>
-      </div>
+          <button onClick={() => {
+            console.log(item)
+            handleDelTodos(item)}}>
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        </div>
       </li>
     ) 
 }
